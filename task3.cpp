377f7c76bc418e1077188fe5a3f82aa173b854e3
ByteOfInterest <anastasiia.zet@gmail.com>
2026-03-16 23:05:07 +0100

    Fixed the comments

--- task3.cpp
+++ task3.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 int main() {
-    // Initialize array with 10 integers (e.g., multiples of 10)
+    // Initialize array with 10 integers
     int arr[10] = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
 
     int index, newValue;
